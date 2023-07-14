5430663ec62359e0f6a0c2ffac7ef9e891f2c4ee
Azamat <alkuat1511@gmail.com>
2023-07-14 16:57:45 +0300

    Draft Join v1.2

--- src/TripCollection.tsx
+++ src/TripCollection.tsx
@@ -2,8 +2,10 @@ import TripBlock from "./TripBlock.tsx";
 import useFetch from "./UseFetch.ts";
 import {SERVER_URL} from "./MainView.tsx";
 import {injectIntl, IntlShape} from "react-intl";
+import { useState, useEffect } from 'react';
 
 export type singleTripDescription = {
+    "id" : number,
     "admin_username": string,
     "admin_id": number,
     "is_driver": boolean,
@@ -14,9 +16,16 @@ export type singleTripDescription = {
     "to_point": number,
     "description": string
 }
+ export type singleJoinDescription = {
+    "status": string,
+}
 export type serverAdjacentTripsResponse =
     {
         data: singleTripDescription[] | null
+    }
+export type serverJoinTripsResponse =
+    {
+        data: singleJoinDescription | null
     }
 export type serverAdjacentTripsRequest = {
     "left_timestamp": string, // new Date()-parsable string
@@ -32,34 +41,48 @@ function TripCollection(props: {
     token: string,
     filters: serverAdjacentTripsRequest
 }) {
-    async function applyJoin() {
-        // if ()
-        // else {
-        //     fetch(`${SERVER_URL}/api/v1/trip/?token=${props.token}`, {
-        //         method: 'post',
-        //         headers: {
-        //             'Accept': 'application/json',
-        //             'Content-Type': 'application/json'
-        //         },
+    const [joinTripId, setJoinTripId] = useState<number|null>(null);
 
-        //         body: JSON.stringify({
-        //             trip_id: index,
-        //         })
-        //     });
-        // }
-    }
     const {
-        data,
-        errorMessage,
-        hasError,
-        isLoading,
+        data: tripsData,
+        errorMessage : tripsErrorMessage,
+        hasError : tripsHasError,
+        isLoading : tripsIsLoading,
     } = useFetch<serverAdjacentTripsResponse>(`${SERVER_URL}/api/v1/trip/adjacent?token=${props.token}`, JSON.stringify(props.filters), "put", false)
 
+    const JOIN_TRIP_URL = joinTripId !== null 
+    ? `${SERVER_URL}/api/v1/user/join_trip/req/${joinTripId}?token=${props.token}`
+    : null;
+    
+    const {
+        data: joinsData,
+        errorMessage : joinsErrorMessage,
+        hasError : joinsHasError,
+        isLoading : joinsIsLoading,
+    } = useFetch<serverJoinTripsResponse>(JOIN_TRIP_URL!, null , "get", false)
+
+    useEffect(() => {
+        if (joinTripId === null) {
+            return;
+        }
+        console.log(joinTripId);
+        // Call the API here
+        console.log(joinsData);
+        console.log(joinsErrorMessage);
+        console.log(joinsHasError);
+        console.log(joinsIsLoading);
+        console.log(JOIN_TRIP_URL);
+    }, [joinTripId]);
+    const applyJoin = (index:number) => {
+        console.log(index);
+        setJoinTripId(index);
+    };    
     return <div className="results">
-        {hasError ? errorMessage :
-            isLoading || data == null ? props.intl.formatMessage({id: "loading_trips"}) :
-                data.data == null ? props.intl.formatMessage({id: "no_trip_matches"}) :
-                    data.data.map((trip, index) => (
+
+        {tripsHasError ? tripsErrorMessage :
+            tripsIsLoading || tripsData == null ? props.intl.formatMessage({id: "loading_trips"}) :
+                tripsData.data == null ? props.intl.formatMessage({id: "no_trip_matches"}) :
+                    tripsData.data.map((trip, index) => (
                         <TripBlock
                             key={index}
                             username={trip.admin_username}
@@ -68,7 +91,7 @@ function TripCollection(props: {
                             date={trip.chosen_timestamp}
                             passengers={trip.places_max - trip.places_taken}
                             extraNote={trip.description}
-                            applyJoin={applyJoin}
+                            applyJoin={() => applyJoin(trip.id)}
                         />
                     ))}
     </div>
